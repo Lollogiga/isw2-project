5b92cdd5bd6e5dcf8b27606f2c7aba5941bbd37d
lollogiga <lorenzo.grande@hotmail.it>
2025-03-22 16:05:40 +0100

    Update to version 1.0.1

---
+++ src/main/java/com/isw2/App.java
@@ -0,0 +1,17 @@
+package com.isw2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        SimpleClass simpleClass = new SimpleClass();
+        simpleClass.sayHello();
+
+    }
+}
---
+++ src/main/java/com/isw2/SimpleClass.java
@@ -0,0 +1,7 @@
+package com.isw2;
+
+public class SimpleClass {
+    public void sayHello() {
+        System.out.println("Hello World from SimpleClass");
+    }
+}
